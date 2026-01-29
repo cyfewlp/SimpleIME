f43214a51f10f17af9adade131f5941715d6d4c6
cyfewlp <2213662140@qq.com>
2026-01-30 01:09:53 +0800

    SimpleIME/i18n: Only first TranslatorHolder##RequestUpdateHandle caller can modify the shared Translator; optimize the `extract_i18n.py` script.

--- extract_i18n.py
+++ extract_i18n.py
@@ -1,7 +1,9 @@
 import os
 import re
 import sys
+
 import toml
+from colorama import Fore, Style
 
 SOURCE_DIR = "./src"
 TRANSLATE_FILE_FORMAT = "contrib/Distribution/translate/translate_{}.toml"
@@ -20,7 +22,6 @@ def extract_keys_from_source(directory):
     return keys
 
 def build_nested_dict(keys):
-    """将 'Settings.Sidebar.Appearance' 转换为嵌套字典"""
     result = {}
     for key in sorted(keys):
         parts = key.split('.')
@@ -33,29 +34,53 @@ def build_nested_dict(keys):
 
 def update_toml(language):
     found_keys = extract_keys_from_source(SOURCE_DIR)
-    new_data = build_nested_dict(found_keys)
+    translate_dict_in_codes = build_nested_dict(found_keys)
 
-    existing_data = {}
+    translate_dict_in_file = {}
     translateFile = TRANSLATE_FILE_FORMAT.format(language)
     if os.path.exists(translateFile):
         with open(translateFile, 'r', encoding='utf-8') as f:
-            existing_data = toml.load(f)
+            translate_dict_in_file = toml.load(f)
 
-    def merge(src, dst):
-        for key, value in src.items():
+    def check_unused_keys(prefix, keys_in_file, keys_in_code):
+        for key, value in keys_in_file.items():
+            current_key = key
+            if prefix != "":
+                current_key = prefix + "." + key
             if isinstance(value, dict):
-                node = dst.setdefault(key, {})
+                if not isinstance(keys_in_code[key], dict):
+                    print(Fore.YELLOW + f"The key: [{current_key}] is incompatible with same key in code.")
+                else:
+                    check_unused_keys(current_key, value, keys_in_code[key])
+            elif key not in keys_in_code:
+                print(Fore.YELLOW + f"The key: [{current_key}] seems unused.")
+
+    check_unused_keys("", translate_dict_in_file, translate_dict_in_codes)
+
+    print(Style.RESET_ALL)
+    print("Reading merge keys to files...")
+
+    missing_count = 0
+    def merge(in_codes, in_files):
+        for key, value in in_codes.items():
+            if isinstance(value, dict):
+                node = in_files.setdefault(key, {})
                 merge(value, node)
             else:
-                if key not in dst or not dst[key]:
-                    dst[key] = value
+                if key not in in_files or not in_files[key]:
+                    missing_count = missing_count + 1
+                    print(Fore.YELLOW + f"The key: [{key}] missing in file.")
+                    in_files[key] = value
 
-    merge(new_data, existing_data)
+    merge(translate_dict_in_codes, translate_dict_in_file)
+    print(Style.RESET_ALL)
 
-    with open(translateFile, 'w', encoding='utf-8') as f:
-        toml.dump(dict(sorted(existing_data.items())), f)
-
-    print(f"Already sync {len(found_keys)} term to translate file: {translateFile}!")
+    if missing_count > 0:
+        with open(translateFile, 'w', encoding='utf-8') as f:
+            toml.dump(dict(sorted(translate_dict_in_file.items())), f)
+        print(f"Already sync {missing_count} missing term to translate file: {translateFile}!")
+    else:
+        print(Fore.GREEN + f"All translate keys is satisfied!")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
